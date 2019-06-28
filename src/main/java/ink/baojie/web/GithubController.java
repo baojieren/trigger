5f08c0507e6521781ecb6f46564f62367077a1de
baojieren <2013142083@qq.com>
2019-06-28 22:48:48 +0800

    ojbk

--- src/main/java/ink/baojie/web/GithubController.java
+++ src/main/java/ink/baojie/web/GithubController.java
@@ -4,15 +4,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 @RestController
 @Slf4j
 public class GithubController {
 
     @PostMapping("/trigger")
-    public void githubTrigger(HttpServletRequest request) {
-        log.info("github触发器来了");
-        System.out.println(request.toString());
+    public void githubTrigger() {
+        log.info("Github触发器来了");
+        String shellPath = "/baojie/gitbook/docs/gitbook.sh";
+        try {
+            int result = Runtime.getRuntime().exec(shellPath).waitFor();
+            // BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            // StringBuilder sb = new StringBuilder();
+            // String line;
+            // while ((line = br.readLine()) != null) {
+            //     sb.append(line).append("\n");
+            // }
+            // log.info("脚本执行日志:{}", sb.toString());
+            log.info("执行脚本结果:{}", result);
+        } catch (Exception e) {
+            log.error("执行脚本出错");
+            e.printStackTrace();
+        }
     }
 }
