08353620dd5a5fc78e9cc37f258d7a37af824980
baojieren <2013142083@qq.com>
2019-06-29 16:02:02 +0800

    ojbk

--- src/main/java/ink/baojie/web/GithubController.java
+++ src/main/java/ink/baojie/web/GithubController.java
@@ -1,29 +1,28 @@
 package ink.baojie.web;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 public class GithubController {
 
     @PostMapping("/trigger")
     public void githubTrigger() {
-        log.info("Github触发器来了");
+        System.out.println("Github触发器来了");
         String shellPath = "/baojie/gitbook/gitbook.sh";
         try {
-            int result = Runtime.getRuntime().exec(shellPath).waitFor();
+            Process process = Runtime.getRuntime().exec(shellPath);
+            int result = process.waitFor();
             // BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
             // StringBuilder sb = new StringBuilder();
             // String line;
             // while ((line = br.readLine()) != null) {
             //     sb.append(line).append("\n");
             // }
-            // log.info("脚本执行日志:{}", sb.toString());
-            log.info("执行脚本结果:{}", result);
+            // System.out.println("执行脚本日志: " + sb.toString());
+            System.out.println("执行脚本结果: " + result);
         } catch (Exception e) {
-            log.error("执行脚本出错");
+            System.out.println("执行脚本出错了");
             e.printStackTrace();
         }
     }
